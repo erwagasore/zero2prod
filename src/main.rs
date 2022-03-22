d0ccaa3603e8ca4fad3ee06506e6849329a33372
Eugene Rwagasore <rwagasore@gmail.com>
2022-03-22 07:10:17 +0100

    Added `/health_check` route, implemented as anonymous function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, zero2prod!");
+use tide::{Response, StatusCode};
+
+#[async_std::main]
+async fn main() -> tide::Result<()> {
+    let mut app = tide::new();
+    app.at("/health_check").get(|_| async { Ok(Response::new(StatusCode::Ok)) });
+    app.listen("0.0.0.0:6000").await?;
+    Ok(())
 }
